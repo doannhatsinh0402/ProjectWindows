700beb2b8b90556c7b15047001152be65a1865f2
doannhatsinh <doannhatsinh0402@gmail.com>
2021-06-12 21:43:13 +0700

    Remove test GUI

--- QuanLySinhVien/QuanLySinhVien.GUI/Form1.cs
+++ QuanLySinhVien/QuanLySinhVien.GUI/Form1.cs
@@ -18,30 +18,5 @@ namespace QuanLySinhVien.GUI
             InitializeComponent();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            dataGridView1.DataSource = KhoaHocBUS.Instance.GetAll().ToList();
-        }
-
-        private void btnXoa_Click(object sender, EventArgs e)
-        {
-            KhoaHocBUS.Instance.Delete("K19_CNTT");
-        }
-
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void btnUpdate_Click(object sender, EventArgs e)
-        {
-            DateTime namBatDau = DateTime.Parse("3/2/2011");
-            DateTime namKetThuc = DateTime.Parse("4/5/2020");
-            KhoaHocBUS.Instance.Update("K1_KTDL", namBatDau, namKetThuc);
-        }
     }
 }
